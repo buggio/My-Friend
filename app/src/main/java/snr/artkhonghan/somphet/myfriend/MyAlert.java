f879ce24a5ec622e903c69a3df48eb7dfb63d23f
buggio <s_artkonghan@hotmail.com>
2016-09-17 15:02:40 +0700

    finish 17

---
+++ app/src/main/java/snr/artkhonghan/somphet/myfriend/MyAlert.java
@@ -0,0 +1,42 @@
+package snr.artkhonghan.somphet.myfriend;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by Phet on 17/09/2016.
+ */
+public class MyAlert {
+
+    // Explicit
+    private Context context;
+    private int anInt;
+    private String titleString, messageString;
+
+    public MyAlert(Context context, int anInt, String titleString, String messageString) {
+        this.context = context;
+        this.anInt = anInt;
+        this.titleString = titleString;
+        this.messageString = messageString;
+    }   // Constructor
+
+    public void myDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(anInt);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+
+    }   // MyDialog
+
+}   // Main Class
--- app/src/main/java/snr/artkhonghan/somphet/myfriend/SignUpActivity.java
+++ app/src/main/java/snr/artkhonghan/somphet/myfriend/SignUpActivity.java
@@ -2,15 +2,58 @@ package snr.artkhonghan.somphet.myfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    // Explicit
+    private EditText nameEditText, userEditText, passwordEditText, rePasswordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton maleRadioButton, femaleRadioButton;
+    private ImageView imageView;
+
+    private String nameString, userString, passwordString, rePasswordString, sexString, imageString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }   //Main Method
+
+        // Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        rePasswordEditText = (EditText) findViewById((R.id.editText4));
+        radioGroup = (RadioGroup) findViewById(R.id.ragSex);
+        maleRadioButton = (RadioButton) findViewById(R.id.radioButton);
+        femaleRadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+    }   // Main Method
+
+    public void clickSignUpSign(View view) {
+
+        //Get Value From Edit Text
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+        rePasswordString = rePasswordEditText.getText().toString().trim();
+
+        // Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("") || rePasswordString.equals("")) {
+            // Have Space
+            MyAlert myAlert = new MyAlert(this, R.drawable.doremon48, "มีช่องว่าง", "กรุณากรอกทุกช่องคะ");
+            myAlert.myDialog();
+
+        }
 
 
+    }   // clickSign
 
-}   //Main Class
+
+}   // Main Class
